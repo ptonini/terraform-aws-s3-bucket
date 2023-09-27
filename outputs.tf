978a4fb580b25db0ab65e6e7c9fb2030460dea66
ptonini <pedro.tonini@hotmail.com>
2023-09-27 18:29:09 -0300

    fix: removed role outputs

--- outputs.tf
+++ outputs.tf
@@ -9,18 +9,3 @@ output "access_policy_statements" {
 output "policy_arn" {
   value = try(module.policy[0].this["arn"], null)
 }
-
-output "role" {
-  value     = try(module.role[0].this, null)
-  sensitive = true
-}
-
-output "vault_role_name" {
-  value     = try(module.role[0].vault_role_name, null)
-  sensitive = true
-}
-
-output "vault_role_path" {
-  value     = try(module.role[0].vault_role_path, null)
-  sensitive = true
-}
